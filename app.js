d08548bd5c1a10b3d3f60f7d3ddadc6306247e32
Clarity-89 <homes89@ukr.net>
2015-08-21 19:56:02 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Alex on 21/08/2015.
+ */
+(function ($) {
+    $('#learn_more').click(function () {
+        $("html, body").animate({scrollTop: "1000px"});
+
+    });
+
+})(jQuery);
