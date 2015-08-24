528290b4661dcbb0b2e851ae42186f0491a50608
Clarity-89 <homes89@ukr.net>
2015-08-24 22:07:19 +0300

    Added responsive navbar and scrolling

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
  * Created by Alex on 21/08/2015.
  */
 (function ($) {
-   //jQuery to collapse the navbar on scroll
-    $(window).scroll(function() {
+    //jQuery to collapse the navbar on scroll and make it transparent/opaque
+    $(window).scroll(function () {
         if ($(".navbar").offset().top > 50) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
+            $('.navbar-default').css({"background": "white"});
         } else {
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            $('.navbar-default').css({"background": "transparent"});
         }
     });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top
@@ -29,7 +31,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
