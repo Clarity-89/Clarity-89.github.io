3181e6d534cbb8db14970bc8b23a15295290be5a
Clarity-89 <homes89@ukr.net>
2015-08-27 23:07:11 +0300

    Contact page

--- app.js
+++ app.js
@@ -7,9 +7,11 @@
         if ($(".navbar").offset().top > 50) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
             $('.navbar-default').css({"background": "white"});
+            $('.navbar .nav > li > a, .navbar-default .navbar-brand').css({color: 'black'})
         } else {
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
             $('.navbar-default').css({"background": "transparent"});
+            $('.navbar .nav > li > a, .navbar-default .navbar-brand').css({color: 'white'})
         }
     });
 
